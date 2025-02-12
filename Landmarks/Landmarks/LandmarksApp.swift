ffb2f61163af48cb14db1076e68d7077c72ab675
Renuka <renukapandey90@gmail.com>
2025-02-12 17:54:23 +0530

    created swift ui project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Renuka Pandey on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
